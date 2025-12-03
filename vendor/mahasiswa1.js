6898757b1f03b7cf36a601a6666a3f46d152df34
yesynuri <yesynuritesya9876@gmail.com>
2025-12-03 16:09:33 +0700

    baru

--- vendor/mahasiswa1.js
+++ vendor/mahasiswa1.js
@@ -1,42 +1,14 @@
-const express = require('express');
-const router = express.Router();
-
-// Data Vendor A (Warung)
-const data = [
-    { 
-        "kd_produk": "A001", 
-        "nm_brg": "Kopi Bubuk 100g", 
-        "hrg": "15000", 
-        "ket_stok": "ada" 
-    },
-    { 
-        "kd_produk": "A002", 
-        "nm_brg": "Gula Merah", 
-        "hrg": "12500", 
-        "ket_stok": "habis" 
-    },
-    { 
-        "kd_produk": "A003", 
-        "nm_brg": "Minyak", 
-        "hrg": "22000", 
-        "ket_stok": "ada" 
-    },
-    { 
-        "kd_produk": "A004", 
-        "nm_brg": "Beras", 
-        "hrg": "14500", 
-        "ket_stok": "ada" 
-    },
-    { 
-        "kd_produk": "A005", 
-        "nm_brg": "Kacang Hijau", 
-        "hrg": "15000", 
-        "ket_stok": "habis" 
-    }
+module.exports = [
+  {
+    kd_produk: "A001",
+    nm_brg: "Kopi Bubuk 100g",
+    hrg: "15000",
+    ket_stok: "ada"
+  },
+  {
+    kd_produk: "A002",
+    nm_brg: "Gula Merah",
+    hrg: "12500",
+    ket_stok: "habis"
+  }
 ];
-
-router.get('/', (req, res) => {
-    res.json(data);
-});
-
-module.exports = router;
