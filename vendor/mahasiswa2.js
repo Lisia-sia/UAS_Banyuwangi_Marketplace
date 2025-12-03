95270576611f9c60b4a838596cf6c7d19d0abcd2
wafhi <wafhinikma@gmail.com>
2025-12-03 14:09:39 +0700

    menambah data mahasiswa2

--- vendor/mahasiswa2.js
+++ vendor/mahasiswa2.js
@@ -3,8 +3,40 @@ const router = express.Router();
 
 // Data Vendor B (Distro)
 const data = [
-    { "sku": "TSHIRT-001", "productName": "Kaos Ijen Crater", "price": 75000, "isAvailable": true },
-    { "sku": "TSHIRT-002", "productName": "Jaket Kawah", "price": 150000, "isAvailable": false }
+    { 
+        "sku": "TSHIRT-001", 
+        "productName": "Kaos Ijen Crater", 
+        "price": 75000, 
+        "isAvailable": true 
+    },
+
+    { 
+        "sku": "TSHIRT-002", 
+        "productName": "Jaket Kawah", 
+        "price": 150000, 
+        "isAvailable": false 
+    },
+
+    { 
+        "sku": "TSHIRT-003", 
+        "productName": "Hoodie Sunrise", 
+        "price": 180000, 
+        "isAvailable": true 
+    },
+
+    { 
+        "sku": "TSHIRT-004", 
+        "productName": "Sweater Blue Mountain", 
+        "price": 130000, 
+        "isAvailable": true 
+    },
+
+    { 
+        "sku": "TSHIRT-005", 
+        "productName": "Polo Shirt Volcano Series", 
+        "price": 95000, 
+        "isAvailable": false 
+    }
 ];
 
 router.get('/', (req, res) => {
@@ -12,3 +44,4 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
