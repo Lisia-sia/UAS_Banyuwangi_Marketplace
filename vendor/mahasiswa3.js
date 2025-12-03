d64d6a8326b6bcadc03b9e65cc13146eaa454022
Devi Khalimatus S. <devihalimatus444@gmail.com>
2025-12-03 14:11:44 +0700

    menambah data baru

--- vendor/mahasiswa3.js
+++ vendor/mahasiswa3.js
@@ -27,6 +27,48 @@ const data = [
             "tax": 500 
         }, 
         "stock": 0 
+    },
+
+    
+    { 
+        "id": 503, 
+        "details": {
+            "name": "Ayam Penyet",
+            "category": "Food"
+        },
+        "pricing": {
+            "base_price": 25000,
+            "tax": 2500
+        },
+        "stock": 30
+    },
+
+    
+    { 
+        "id": 504, 
+        "details": {
+            "name": "Jus Alpukat",
+            "category": "Drink"
+        },
+        "pricing": {
+            "base_price": 12000,
+            "tax": 1200
+        },
+        "stock": 15
+    },
+
+    
+    { 
+        "id": 505, 
+        "details": {
+            "name": "Mie Goreng",
+            "category": "Food"
+        },
+        "pricing": {
+            "base_price": 18000,
+            "tax": 1800
+        },
+        "stock": 25
     }
 ];
 
