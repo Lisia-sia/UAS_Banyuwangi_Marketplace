edf036dd170ccd5f90b083c5534448f4bcd9f60a
HanKy <jihanmartha02@gmail.com>
2025-12-04 19:50:36 +0700

    memperbaiki field

--- server.js
+++ server.js
@@ -346,7 +346,10 @@ app.get("/api/integrated-products/db", async (req, res) => {
   }
 });
 
+// --------------------
+// START SERVER / export for Vercel
+// --------------------
 if (require.main === module) {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
-module.exports = app;
+module.exports = app; 
