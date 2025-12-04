d05135e479fa72da041381894d6040e45bcd1ee5
HanKy <jihanmartha02@gmail.com>
2025-12-04 15:09:26 +0700

    memperbaiki server mhs 4 dan integrasinya

--- integrator/mahasiswa4.js
+++ integrator/mahasiswa4.js
@@ -1,9 +1,12 @@
+const db = require("../db.js");
+
 function normalize(dataA, dataB, dataC) {
   const normA = dataA.map(item => {
     let harga = parseInt(item.hrg);
     harga = Math.floor(harga * 0.9); // diskon 10%
+
     return {
-      id: item.kd_produk,
+      original_id: item.kd_produk,
       product_name: item.nm_brg,
       price_final: harga,
       stock_status: item.ket_stok === "habis" ? "Habis" : "Tersedia",
@@ -12,26 +15,43 @@ function normalize(dataA, dataB, dataC) {
   });
 
   const normB = dataB.map(item => ({
-    id: item.sku,
+    original_id: item.sku,
     product_name: item.product_name,
     price_final: item.price,
     stock_status: item.is_available ? "Tersedia" : "Habis",
     vendor: "B"
   }));
 
-  const normC = dataC.map(item => {
-    let name = item.name;
-    if (item.category === "Food") name += " (Recommended)";
-    return {
-      id: item.product_code.toString(),
-      product_name: name,
-      price_final: item.base_price + item.tax,
-      stock_status: item.stock > 0 ? "Tersedia" : "Habis",
-      vendor: "C"
-    };
-  });
+  const normC = dataC.map(item => ({
+    original_id: item.product_code.toString(),
+    product_name: item.name,
+    price_final: item.base_price + item.tax,
+    stock_status: item.stock > 0 ? "Tersedia" : "Habis",
+    vendor: "C"
+  }));
 
   return [...normA, ...normB, ...normC];
 }
 
-module.exports = { normalize };
+async function saveIntegrated(data) {
+  await db.query(`DELETE FROM integrated_products`);
+
+  for (let item of data) {
+    await db.query(
+      `INSERT INTO integrated_products 
+      (original_id, product_name, price_final, stock_status, vendor)
+       VALUES ($1, $2, $3, $4, $5)`,
+      [
+        item.original_id,
+        item.product_name,
+        item.price_final,
+        item.stock_status,
+        item.vendor
+      ]
+    );
+  }
+
+  return { message: "Integrated data saved!" };
+}
+
+module.exports = { normalize, saveIntegrated };
